3cafe61441550ededb0e9bdaba527df1bca72a0a
scarlqq <song.qingqing1001@gmail.com>
2022-01-18 23:25:40 -0800

    Add function docstring of visualize_outliers

---
+++ R/visualize_outliers.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' @param dataframe A target dataframe where the function is performed.
+#' @param columns The target columns where the function needed to be performed. Default is None, the function will check all columns.
+#' @param type The method of plotting the distribution.
+#'
+#' @return A ggplot with data distribution.
+#' @export
+#'
+#' @examples
+#' df <- tibble(cola=c(1:5), colb=c(6:10), colc=c(11:15))
+#' visualize_outliers(x, columns=c("cola", "colb"), type="violin")
+visualize_outliers <- function(dataframe, columns=NA, type="violin") {
+
+}
